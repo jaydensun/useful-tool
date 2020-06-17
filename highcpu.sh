1d502f2fe62d80a7333fc94f300855ef1d1cdd95
unwillingman <at3134165@126.com>
2020-06-17 18:15:52 +0800

    Update highcpu.sh

--- highcpu.sh
+++ highcpu.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-[ $# -ne 2 ] && exit 1
+if [ $# -ne 2 ] ; then
+  echo -e "usage: sh highcpu.sh [pid] [num]\nSample: sh highcpu.sh 123 3 \n   means print 123's top 3 threads."
+  exit 1
+fi
 jstack $1 >/tmp/jstack.log
-top -H -p $1 -b -n1 | grep PID -A$2 | grep -v PID | awk '{print$1,$9}' >/tmp/topH.log
+top -H -p $1 -b -n1 | grep -v "^ $1" | grep PID -A$2 | grep -v PID | awk '{print$1,$9}' >/tmp/topH.log
 cat /tmp/topH.log | while read tid cpu;do
   xtid=`printf "%x\n" $tid`
   echo -e "\033[31m========================$xtid $cpu%\033[0m"
