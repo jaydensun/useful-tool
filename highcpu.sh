845fcfc58570aefe68a80d0b7bc18dbb38008f9b
unwillingman <at3134165@126.com>
2020-06-28 18:35:41 +0800

    Update highcpu.sh

--- highcpu.sh
+++ highcpu.sh
@@ -7,6 +7,6 @@ jstack $1 >/tmp/jstack.log
 top -H -p $1 -b -n1 | grep -v "^ $1" | grep PID -A$2 | grep -v PID | awk '{print$1,$9}' >/tmp/topH.log
 cat /tmp/topH.log | while read tid cpu;do
   xtid=`printf "%x\n" $tid`
-  echo -e "\033[31m========================$xtid $cpu%\033[0m"
+  echo -e "\033[31m========================$tid($xtid) $cpu%\033[0m"
   cat /tmp/jstack.log | sed -n -e "/0x$xtid/,/^$/ p"
 done
